d5eb9b1f49a4578b5d784ad00a22346c9e9bcd99
JoakinMacAuliffe <jfmacauliffe@gmail.com>
2026-01-02 23:35:46 -0300

    feat: implement session authentication and redirect for unauthenticated users

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,17 @@
 import { db } from "../lib/db";
 import { semestres } from "../db/schema";
 import { desc } from "drizzle-orm";
+import { auth } from "../auth.js"
 import SemesterList from "../components/semester_list";
 
 const Home = async () => {
+  
+  const session = await auth();
+
+  if (!session?.user) {
+    redirect("/login");
+  }
+
   const allSemesters = await db
     .select()
     .from(semestres)
